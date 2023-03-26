c7809339b7a9c1dd4fffb52252122155306bdeca
Sébastien Vanvelthem <belgattitude@gmail.com>
2023-03-26 19:48:59 +0200

    chore: improve cli output

--- scripts/check-git-pristine.sh
+++ scripts/check-git-pristine.sh
@@ -2,9 +2,10 @@
 # This script helps to identify unexpected changes during steps
 if [ $(git status --porcelain | wc -c) -gt 0 ];
 then
-    printf "[ERROR]: The git repository state changed after the previous operation.\n"
+    printf "[Error]: The git repository state changed after the previous operation.\n"
     printf "         Here's the output of git status.\n\n"
     git status
     printf "\nHINT: Did you forget to run the codegen on changed packages ?\n"
     exit 1
 fi
+printf "[Success] The git repository state didn't change after the previous operation\n"
